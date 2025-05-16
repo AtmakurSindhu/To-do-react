9687107f4f6ea6027ecbfdaa866fb99b2cd5bf37
Sindhu Atmakur <atmakursindhu@gmail.com>
2025-05-16 13:39:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,7 @@ import Home from './Pages/Home.jsx';
 function App() {
   return (
     <>
-      <h1
-        style={{
-          textAlign: 'center',
-          fontSize: '2.5rem',
-          fontWeight: 'bold',
-          color: '#ffffff',
-          backgroundColor: '#6a0dad', // purple background
-          marginTop: '20px',
-          padding: '15px',
-          borderRadius: '10px',
-          fontFamily: 'Segoe UI, Tahoma, Geneva, Verdana, sans-serif'
-        }}
-      >
+      <h1 className="mh">
         To-Do App
       </h1>
       <Home />
